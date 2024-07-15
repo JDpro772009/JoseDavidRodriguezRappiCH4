95c6d7b64a785835f3f8db91642787a31c1da401
jhoselerodriguez <jhoselerodriguez@gmail.com>
2024-07-15 16:41:48 -0500

    guardado

--- EjerciciosEventos/script.js
+++ EjerciciosEventos/script.js
@@ -142,25 +142,91 @@ borrar.addEventListener("click", ()=>{borrarText()})
 guardar.addEventListener("click", ()=>{agregarNota()})
 
 
-let buscador = document.querySelector(".buscar")
 let realizada = document.querySelector(".realizadas")
 
+let completas = []
 function marcarRealizada(id){
     if(notas[id-1].realizada){
         notas[id-1].realizada = false
+        completas[id-1] = ""
     }else{
         notas[id-1].realizada = true
+        completas[id-1] = notas[id-1]
     }
-    console.log(notas[id-1])
+    console.log(completas)
     
 }
 
-function filtrar(array){
-    let completas = []
-    for(let i = 0; i < array.length; i++){
-        if(array[i].realizada){
-            completas.push(array[i])
+function filtrar(){
+    
+    if(realizada.checked){
+        console.log(completas)
+        cajaP.innerHTML = ""
+    for(let i = 0; i < completas.length; i++){
+       
+        if(completas[i] == ""){
+            console.log("nada")
+        } else{
+            let caja = document.querySelector(".cartas")
+        let carta = document.createElement("div")
+            carta.setAttribute("class", "card text-center cards")
+            carta.innerHTML = `<div class="card-header">
+                              <input onClick="marcarRealizada(${completas[i].id})" type="checkbox" ${completas[i].realizada? "checked": ""} class:"chekear">
+                              <label for="chekear">${completas[i].titulo}</label>
+                            </div>
+                            <div class="card-body">
+                              <p class="card-text">${completas[i].texto}</p>
+                            </div>
+                              <button type="button" class="btn btn-danger borrarN"  onclick="borrarNota(${completas[i].id})">Borrar</button>`
+                              
+        
+            caja.appendChild(carta)
+        }
+    
+    }
+
+
+}else{
+    cajaP.innerHTML = ""
+    for(let i = 0; i < notas.length; i++){
+       
+        if(notas[i] == ""){
+            
+        } else{
+            let caja = document.querySelector(".cartas")
+        let carta = document.createElement("div")
+            carta.setAttribute("class", "card text-center cards")
+            carta.innerHTML = `<div class="card-header">
+                              <input onClick="marcarRealizada(${notas[i].id})" type="checkbox" ${notas[i].realizada? "checked": ""} class:"chekear">
+                              <label for="chekear">${notas[i].titulo}</label>
+                            </div>
+                            <div class="card-body">
+                              <p class="card-text">${notas[i].texto}</p>
+                            </div>
+                              <button type="button" class="btn btn-danger borrarN"  onclick="borrarNota(${notas[i].id})">Borrar</button>`
+                              
+        
+            caja.appendChild(carta)
+        }
+    }    
+}
+}
+realizada.addEventListener("click", filtrar)
+
+
+let buscador = document.querySelector(".buscar")
+
+let incluyen = []
+
+function buscar(){
+
+    for(let i = 0; i < notas.length;i++){
+        if(notas[i].titulo.includes(buscador.value)){
+            console.log("si esta");
+            console.log(notas[i]);
         }
     }
-    console.log(completas)
+
 }
+
+buscador.addEventListener("keyup", buscar )
